3b63b9bec2f751f21341c4c4b7aadc3a1c3baa51
= <awcrotwell@gmail.com>
2020-10-18 10:40:31 -0400

    Migration & jennifer cli

--- src/launch/cli/helpers/migration.cr
+++ src/launch/cli/helpers/migration.cr
@@ -1,9 +1,9 @@
 module Launch::CLI::Helpers::Migration
-  private def create_table_field(field : Field)
+  private def create_table_field(field : Field) : String
     "t.#{field.db_type} :#{field.name}"
   end
 
-  private def create_table_fields
+  private def create_table_fields : String
     @fields.map { |field| create_table_field(field) }.join("\n      ")
   end
 end
