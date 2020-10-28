cf65efe3e1a258494682a2714c5602f07716907e
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-10-27 20:28:50 -0400

    Better React/Typescript Generation & Scaffolding (#12)

--- src/launch/cli/templates/app/src/pages/index.tsx
+++ src/launch/cli/templates/app/src/pages/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import axios from 'axios'
 import HelloLaunch from '../components/HelloLaunch';
+import ServerResponse from '../types/ServerResponse';
 
-const Home = () => (
-  <HelloLaunch />
+const Home = ({ message }: { message: string }) => (
+  <HelloLaunch message={message} />
 );
 
 export default Home;
+
+interface HomeResponse extends ServerResponse {
+  data: {
+    message: string
+  }
+}
+
+export async function getServerSideProps() {
+  const props = { message: '' };
+  await axios.get('http://localhost:3001')
+    .then((res: HomeResponse) => {
+      if (res.status === 200) {
+        props.message = res.data.message
+      } else {
+        props.message = 'Some error has occurred.'
+      }
+    });
+  return {
+    props,
+  };
+}
